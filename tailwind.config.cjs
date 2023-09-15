e8b977088cf63c8a0a7fac2c8690e574904024ee
itsmattsoria <matt.soria@hey.com>
2023-09-15 08:35:53 -0500

    Project init

---
+++ tailwind.config.cjs
@@ -0,0 +1,40 @@
+const corePlugins = require('./theme/config/corePlugins');
+const typography = require('@tailwindcss/typography');
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js}'],
+  theme: {
+    extend: {
+      body: {
+        margin: '0.25rem',
+      },
+      colors: {
+        current: 'currentColor',
+        white: '#FFEFCF',
+        black: '#140C10',
+        orange: '#E2703A',
+        tan: '#EEB76B',
+        primary: 'var(--colorPrimary)',
+        secondary: 'var(--colorSecondary)',
+        highlight: 'var(--colorHighlight)',
+      },
+      fontFamily: {
+        sans: ['ATC Overlook', ...defaultTheme.fontFamily.sans],
+      },
+      borderRadius: {
+        DEFAULT: '0.5rem',
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            color: 'var(--colorPrimary)',
+          },
+        },
+      },
+    },
+  },
+  corePlugins,
+  plugins: [typography],
+};
