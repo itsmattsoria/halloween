8bf257c89d51be639404bd39b599fd79a5d588df
itsmattsoria <matt.soria@hey.com>
2023-09-17 12:58:15 -0500

    Add image functionality, refactor year template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://halloween.mattsoria.com',
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), mdx()]
 });
