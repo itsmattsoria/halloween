5d549a96e52e5fffe3b51fb5bebb6781ebf7038b
itsmattsoria <matt.soria@hey.com>
2023-09-29 14:38:45 -0500

    Add hybrid mode, random top rated movies on index

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
 
-import mdx from "@astrojs/mdx";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://halloween.mattsoria.com',
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), mdx()]
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    mdx(),
+  ],
+  output: 'hybrid',
+  adapter: vercel(),
 });
